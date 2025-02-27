736e12436abc0813bda95df97c1bb888ea31645c
adammnich <ar.mnich@student.uw.edu.pl>
2025-02-27 22:11:54 +0100

    zmiana

--- zaj2_zad_3.R
+++ zaj2_zad_3.R
@@ -1,19 +1,25 @@
+
 # Tworzenie chmury słów za pomocą pakietu wordcloud
-install.packages("wordcloud")
+#install.packages("wordcloud")
 library(wordcloud)
+
+#install.packages("qdap")
+library(qdap)
 
 # Wczytaj dane tekstowe
 # Wczytaj plik tekstowy z lokalnego dysku
 text <- readLines(file.choose())
 text
 
-
-frequent_terms <- freq_terms(text)
-frequent_terms
-frequent_terms <- freq_terms(text, stopwords = Top200Words)
-plot(frequent_terms)
 # Utwórz chmurę słów
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ)
+
+
+# Zmiana wartości min.freq i max.words w celu wyświetlenia mniejszej/większej liczby słów.
+# min.freq: słowa o częstości poniżej tej wartości nie będą wyświetlane
+# max.words: maksymalna liczba słów do wyświetlenia
+
+
 
 # Ograniczenie liczby słów w chmurze poprzez określenie minimalnej częstości
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4)
@@ -22,19 +28,21 @@ wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4)
 
 # Ograniczenie liczby słów w chmurze poprzez określenie maksymalnej liczby słów
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ, max.words = 5)
+
+
+
+
+
 # Dodanie różnych palet kolorystycznych
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(9,"Blues"))
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(9,"Reds"))
 wordcloud(frequent_terms$WORD, frequent_terms$FREQ, min.freq = 4, colors = brewer.pal(9,"Greens"))
 
 
-
-
-
-
-
-
-
+frequent_terms <- freq_terms(text)
+frequent_terms
+frequent_terms <- freq_terms(text, stopwords = Top200Words)
+plot(frequent_terms)
 
 # Optymalizacja i dostosowanie wyników
 # Dodanie koloru do chmury słów dla lepszej wizualizacji
@@ -45,14 +53,8 @@ wordcloud(frequent_terms$WORD, frequent_terms$FREQ, max.words = 5, colors = brew
 ?brewer.pal
 brewer.pal.info
 
-
-
 # Opcje chmury słów
 ?wordcloud
-# Zmiana wartości min.freq i max.words w celu wyświetlenia mniejszej/większej liczby słów.
-# min.freq: słowa o częstości poniżej tej wartości nie będą wyświetlane
-# max.words: maksymalna liczba słów do wyświetlenia
-
 
 
 
